24f16507a89bc752fc30a5af5ef80a83dd636bb8
Asus <Asus@DESKTOP-F2RLVQ4>
2023-11-25 01:03:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+    meow:
+        Console.WriteLine("Введите размерность массивчика >.< :");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] m = new int[n];
+
+        if (n  < 1 || n > 100)
+        {
+            Console.WriteLine("Размерность должна быть 1 <= N <= 100!  Введите новвое значение..");
+            goto meow;
+        }
+        
+        Console.WriteLine("Введите элементы массивва:");
+
+        for (int i = 0; i < n; i++)
+        {
+           m[i] = Convert.ToInt32(Console.ReadLine());
+       
+        }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (m[i] == -m[j])
+                    {
+                       
+                        Console.WriteLine($"Противоположные числа найдены: {m[i]} (индекс {i}) и {m[j]} (индекс {j})");
+                        return;
+                    }
+                }
+            }
+
+     }
+}
